fa2a6a7faf8a2cb51d63c351b8ae9b4b50b21006
Pradeepti Aryal <67222227+pradeepti239@users.noreply.github.com>
2021-11-13 11:10:49 +0545

    added pop up

---
+++ js/main.js
@@ -0,0 +1,112 @@
+//for navigation
+
+function introcircle() {
+    document.querySelector(".introcircle").style.cssText = "background : #AAAAAA; border:none;"
+    document.querySelector(".aboutcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".servicescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".facilitiescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".gallerycircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".testimonialcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".clientscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".autopartscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".contactcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+}
+
+function aboutcircle() {
+    document.querySelector(".introcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".aboutcircle").style.cssText = "background : #AAAAAA; border:none;"
+    document.querySelector(".servicescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".facilitiescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".gallerycircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".testimonialcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".clientscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".autopartscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".contactcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+}
+
+function servicescircle() {
+    document.querySelector(".introcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".aboutcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".servicescircle").style.cssText = "background : #AAAAAA; border:none;"
+    document.querySelector(".facilitiescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".gallerycircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".testimonialcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".clientscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".autopartscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".contactcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+}
+
+function facilitiescircle() {
+    document.querySelector(".introcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".aboutcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".servicescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".facilitiescircle").style.cssText = "background : #AAAAAA; border:none;"
+    document.querySelector(".gallerycircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".testimonialcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".clientscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".autopartscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".contactcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+}
+
+function gallerycircle() {
+    document.querySelector(".introcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".aboutcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".servicescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".facilitiescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".gallerycircle").style.cssText = "background : #AAAAAA; border:none;"
+    document.querySelector(".testimonialcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".clientscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".autopartscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".contactcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+}
+
+function testimonialcircle() {
+    document.querySelector(".introcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".aboutcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".servicescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".facilitiescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".gallerycircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".testimonialcircle").style.cssText = "background : #AAAAAA; border:none;"
+    document.querySelector(".clientscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".autopartscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".contactcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+}
+
+function clientscircle() {
+    document.querySelector(".introcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".aboutcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".servicescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".facilitiescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".gallerycircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".testimonialcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".clientscircle").style.cssText = "background : #AAAAAA; border:none;"
+    document.querySelector(".autopartscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".contactcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+}
+
+function autopartscircle() {
+    document.querySelector(".introcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".aboutcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".servicescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".facilitiescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".gallerycircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".testimonialcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".clientscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".autopartscircle").style.cssText = "background : #AAAAAA; border:none;"
+    document.querySelector(".contactcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+}
+
+function contactcircle() {
+    document.querySelector(".introcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".aboutcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".servicescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".facilitiescircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".testimonialcircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".gallerycircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".clientscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".autopartscircle").style.cssText = "background-color: transparent; border: 2px solid #717171;"
+    document.querySelector(".contactcircle").style.cssText = "background : #AAAAAA; border:none;"
+}
+
+
+
